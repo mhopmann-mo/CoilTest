29179d1e68d48f6635ba5e83b0a5b1f7882040a2
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2021-10-13 21:22:33 +0300

    Not working coil activity

--- app/src/main/java/ru/beryukhov/coiltest/MainActivity.kt
+++ app/src/main/java/ru/beryukhov/coiltest/MainActivity.kt
@@ -3,41 +3,53 @@ package ru.beryukhov.coiltest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ru.beryukhov.coiltest.ui.theme.CoilTestTheme
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import coil.compose.rememberImagePainter
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CoilTestTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+            val url = "https://clipart-db.ru/file_content/rastr/leopard_008.png"
+            Column() {
+                Text("Working")
+                Image(painter = rememberImagePainter(
+                    data = url,
+                    builder = {
+                        placeholder(R.drawable.ic_launcher_background)
+                    }
+                ),
+                    contentDescription = "",
+                    modifier = Modifier.height(200.dp)
+                )
+                Text("Not working")
+                ConstraintLayout(Modifier) {
+                    val (ivBackgroundPoster) = createRefs()
+                    Image(painter = rememberImagePainter(
+                        data = url,
+                        builder = {
+                            placeholder(R.drawable.ic_launcher_background)
+                        }
+                    ),
+                        contentScale = ContentScale.Crop,
+                        contentDescription = "",
+                        modifier = Modifier.constrainAs(ivBackgroundPoster) {
+                            top.linkTo(parent.top)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                            width = Dimension.fillToConstraints//probably this brokes
+                        }
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CoilTestTheme {
-        Greeting("Android")
     }
 }
