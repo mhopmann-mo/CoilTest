b5d41b195542fb12db1dfffbd4d58c0ed1c43e9e
Hopmann, Mathieu-MIGROSONLINE <Mathieu.Hopmann@migrosonline.ch>
2022-03-07 14:32:04 +0100

    Move to coil-compose 2.0.0-rc01 and use AsyncImage

--- app/src/main/java/ru/beryukhov/coiltest/MainActivity.kt
+++ app/src/main/java/ru/beryukhov/coiltest/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
+import coil.compose.AsyncImage
 import coil.compose.rememberImagePainter
 
 class MainActivity : ComponentActivity() {
@@ -19,28 +23,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             val url = "https://clipart-db.ru/file_content/rastr/leopard_008.png"
-            Column() {
+            Column {
                 Text("Working")
-                Image(painter = rememberImagePainter(
-                    data = url,
-                    builder = {
-                        placeholder(R.drawable.ic_launcher_background)
-                    }
-                ),
+                AsyncImage(
+                    model = url,
                     contentDescription = "",
+                    placeholder = painterResource(R.drawable.ic_launcher_background),
+                    contentScale = ContentScale.Crop,
                     modifier = Modifier.height(200.dp)
                 )
+
                 Text("Not working")
                 ConstraintLayout(Modifier) {
                     val (ivBackgroundPoster) = createRefs()
-                    Image(painter = rememberImagePainter(
-                        data = url,
-                        builder = {
-                            placeholder(R.drawable.ic_launcher_background)
-                        }
-                    ),
-                        contentScale = ContentScale.Crop,
+
+                    AsyncImage(
+                        model = url,
                         contentDescription = "",
+                        placeholder = painterResource(R.drawable.ic_launcher_background),
+                        contentScale = ContentScale.Crop,
                         modifier = Modifier.constrainAs(ivBackgroundPoster) {
                             top.linkTo(parent.top)
                             start.linkTo(parent.start)
